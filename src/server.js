206120af5942cab976dcc8b1497dc69914fce297
Rayssa Cadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-04-18 19:28:59 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,30 @@
-import app from './app.js'
+import express from 'express';
+import app from './app.js';
+import cors from 'cors';
+import usuarioController from './src/controllers/usuarioController.js';
+import adminController from './src/controllers/adminController.js';
+import contatoController from './src/controllers/contatoController.js';
+import imagensController from './src/controllers/imagensController.js';
+import noticiasController from './src/controllers/noticiasController.js';
+import parceirosController from './src/controllers/parceirosController.js';
 
-// escolhendo a porta em que o servidor será aberto
+// Importando o express
+
+const app = express();
 const port = 3000
 
-// abrindo o servidor na porta escolhida
-app.listen(port, ()=>{
-    console.log(`Server rodando em http://localhost:${port}/`)
-})
+//O middleware "express.json()" irá converter esse JSON em um objeto JavaScript que pode ser manipulado pelo servidor. 
+app.use(express.json());
+app.use(cors())
+
+usuarioController.getUsauarioController(app)
+adminController.getAdminController(app)
+contatoController.getContatoController(app)
+imagensController.getImagensController(app)
+noticiasController.getNoticiasController(app)
+parceirosController.getParceirosController(app)
+
+//Servidor rodando na porta 3000
+app.listen(`${port}`, () =>
+console.log(`Servidor iniciado na porta ${port}`)
+);
