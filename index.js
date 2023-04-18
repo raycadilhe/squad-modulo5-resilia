9683977e7630ab8a6d7186b79b321e146023d572
Rayssa Cadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-04-18 19:40:41 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
-import cors from 'cors'
-import UsauarioController from './src/controllers/usuarioController.js';
+import cors from 'cors';
+import usuarioController from './src/controllers/usuarioController.js';
+import adminController from './src/controllers/adminController.js';
+import contatoController from './src/controllers/contatoController.js';
+import imagensController from './src/controllers/imagensController.js';
+import noticiasController from './src/controllers/noticiasController.js';
+import parceirosController from './src/controllers/parceirosController.js';
 
 // Importando o express
 
@@ -11,9 +16,12 @@ const port = 3000
 app.use(express.json());
 app.use(cors())
 
-
-UsauarioController.getUsauarioController(app)
-
+usuarioController.getUsauarioController(app)
+adminController.getAdminController(app)
+contatoController.getContatoController(app)
+imagensController.getImagensController(app)
+noticiasController.getNoticiasController(app)
+parceirosController.getParceirosController(app)
 
 //Servidor rodando na porta 3000
 app.listen(`${port}`, () =>
