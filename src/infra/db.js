fe781dc60689ac24508d9f9ed68c8cbef5371f24
Rayssa <raycadilhe@hotmail.com>
2023-04-19 12:51:15 -0300

    att final

--- src/infra/db.js
+++ src/infra/db.js
@@ -2,6 +2,6 @@ import  sqlite3  from "sqlite3";
 
 
 sqlite3.verbose()
-const db = new sqlite3.Database("src/infra/database.db")
+const db = new sqlite3.Database("./src/infra/database.db")
 
 export default db
