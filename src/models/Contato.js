272fa2acebb9910216bcd2e9ae504c4962c4076f
Rayssa Cadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-04-18 21:16:12 -0300

    Update Contato.js

--- src/models/Contato.js
+++ src/models/Contato.js
@@ -1,6 +1,5 @@
 class Contato{
-    constructor(id, nome, sobrenome, email, comentario, conceito, data, hora){
-        this.id = id,
+    constructor(nome, sobrenome, email, comentario, conceito, data, hora){
         this.nome = nome,
         this.sobrenome = sobrenome,
         this.email = email,
